6574b71d10c2fd27f6839756ae04c4e4cb0a5c8f
Hemit Patel <hemitvpatel@gmail.com>
2025-03-30 21:12:58 -0400

    minor Ui updates + /dashaboard made to be authenticated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,29 @@
+"use client"
+
 import { GlowingButton } from '@/components/features/Landing-Page/GlowingButton'
-import { ArrowRight, ChevronDown } from 'lucide-react'
+import { ChevronDown } from 'lucide-react'
+import { useRouter } from 'next/navigation'
 import React from 'react'
 
 const Home = () => {
+
+  const router = useRouter()
   return (
     <div className="flex flex-col items-center justify-center"> {/* Adjusted padding-top to fit navbar */}
-      <section className="w-full min-h-screen bg-gradient-to-br from-blue-50 via-white to-purple-50 flex flex-col items-center pt-32 text-center relative px-4 md:px-6">
+      <section className="w-full min-h-screen bg-gradient-to-br from-blue-50 via-white to-purple-50 flex flex-col items-center pt-36 text-center relative px-4 md:px-6">
         <div className="absolute inset-0 w-full h-full">
           <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[500px] h-[500px] bg-blue-400/20 rounded-full blur-3xl" />
         </div>
 
-        <div className="relative space-y-6">
-          <div className="inline-block rounded-lg bg-blue-100 px-3 py-1 text-sm text-blue-700 backdrop-blur-md border border-blue-200">
-            3,500+ Practice Questions <ArrowRight className="inline h-4 w-4 ml-1" />
-          </div>
-          
-          <div className="flex flex-col text-3xl font-extrabold tracking-tighter sm:text-6xl md:text-7xl lg:text-8xl">
-            <span className="text-black text-6xl">SAT Prep</span>
+        <div className="relative space-y-6">          
+          <div className="flex flex-col text-3xl font-extrabold tracking-tight sm:text-6xl md:text-7xl lg:text-8xl">
+            <span className="text-black text-6xl">The SATs</span>
             <span className=" bg-gradient-to-r from-blue-600 to-blue-400 bg-clip-text text-transparent">Made Easy</span>
           </div>
 
           <div className="w-full">
-            <GlowingButton className="w-full h-14 text-lg font-bold bg-gradient-to-r from-blue-500 to-blue-600 shadow-lg hover:opacity-90">
-              Start Practicing Now
+            <GlowingButton className="w-full h-14 text-lg font-bold bg-gradient-to-r from-blue-500 to-blue-600 shadow-lg hover:opacity-90" onClick={() => router.push("/dashboard")}>
+              Explore DailySAT
             </GlowingButton>
           </div>
         </div>
