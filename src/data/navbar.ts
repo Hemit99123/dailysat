f52a86884df5ec87538cdb44d3d26789f55df116
Hemit Patel <hemitvpatel@gmail.com>
2025-04-01 22:02:43 -0400

    added type safeing for navbar items dataset

--- src/data/navbar.ts
+++ src/data/navbar.ts
@@ -1,5 +1,7 @@
+import { NavItem } from "@/types/navbar";
+
 // Define the menu items for navigation
-export const menuItems = [
+export const menuItems: NavItem[] = [
   { label: "Dashboard", href: "/dashboard"},
   { label: "Math", href: "/math"},
   { label: "Reading & Writing", href:"/reading-writing"},
