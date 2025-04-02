cc728773ff2666fdea5152699a4e14f1359e70b1
Hemit Patel <hemitvpatel@gmail.com>
2025-04-01 22:00:06 -0400

    added dashboard to navbar

--- src/data/navbar.ts
+++ src/data/navbar.ts
@@ -1,5 +1,6 @@
 // Define the menu items for navigation
 export const menuItems = [
+  { label: "Dashboard", href: "/dashboard"},
   { label: "Math", href: "/math"},
   { label: "Reading & Writing", href:"/reading-writing"},
   { label: "About", href: "/about" },
