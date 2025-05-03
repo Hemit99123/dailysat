a23b5afa644e8aaa8d7ee314e8de4d8a9140b2df
Hemit Patel <hemitvpatel@gmail.com>
2025-05-02 22:17:02 -0400

    fixed eslint of '

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -6,7 +6,7 @@ const NotFound = () => {
         Page not found 😕
       </h1>
       <p className="mt-4 text-base text-gray-600">
-        Sorry, we couldn't find the page you're looking for.
+        Sorry, we couldn&apos;t find the page you&apos;re looking for.
       </p>
       <div className="mt-6">
         <a
