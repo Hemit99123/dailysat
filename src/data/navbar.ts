93926baaf6179620e692735431b89f3b3f8eb867
Hemit Patel <hemitvpatel@gmail.com>
2025-05-15 10:29:14 -0400

    added shop to main navbar

--- src/data/navbar.ts
+++ src/data/navbar.ts
@@ -2,9 +2,10 @@ import { NavItem } from "@/types/navbar";
 
 // Define the menu items for navigation
 export const menuItems: NavItem[] = [
-  { label: "Dashboard", href: "/dashboard"},
-  { label: "Study AI", href: "/ai"},
-  { label: "Math", href: "/math"},
-  { label: "Reading & Writing", href:"/reading-writing"},
+  { label: "Dashboard", href: "/dashboard" },
+  { label: "Shop", href: "/shop" },
+  { label: "Study AI", href: "/ai" },
+  { label: "Math", href: "/math" },
+  { label: "Reading & Writing", href:"/reading-writing" },
   { label: "Team", href:"/team" },
 ];
