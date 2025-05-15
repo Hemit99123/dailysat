be69305aa23cbc512aaf160edb765f02243775d6
Hemit Patel <hemitvpatel@gmail.com>
2025-05-14 20:49:50 -0400

    Revert "fix: reformed math and reading section ui + added college api "

--- src/components/features/Questions/Question-UI/Results.tsx
+++ src/components/features/Questions/Question-UI/Results.tsx
@@ -1,15 +1,14 @@
 import { useAnswerCorrectStore } from "@/store/questions";
 import { questionType } from "@/types/sat-platform/questions";
-import React, { MutableRefObject, useEffect, useState } from "react";
-import Image from "next/image";
-import Latex from "react-latex-next";
+import React, { MutableRefObject } from "react";
+import Image from "next/image"
+import { parseContent } from "@/lib/latex";
 
 interface ResultProps {
   answerComponent: MutableRefObject<HTMLDivElement | null>;
   explanation: string | undefined;
   type: questionType;
   imageUrls?: string[];
-  userAnswered?: boolean; // Add prop to check if user has answered
 }
 
 const Result: React.FC<ResultProps> = ({
@@ -17,33 +16,21 @@ const Result: React.FC<ResultProps> = ({
   explanation,
   type,
   imageUrls,
-  userAnswered = false, // Default to false if not provided
 }) => {
   const isAnswerCorrect = useAnswerCorrectStore((state) => state.isAnswerCorrect);
-  const [showExplanation, setShowExplanation] = useState(false);
-
-  // Only show the explanation when the user has answered
-  useEffect(() => {
-    setShowExplanation(userAnswered && isAnswerCorrect !== "none");
-  }, [userAnswered, isAnswerCorrect]);
 
   return (
     <div className="mt-4 pl-7 pb-10" ref={answerComponent}>
-      {showExplanation && (
-        isAnswerCorrect ? (
+      {isAnswerCorrect !== "none" &&
+      
+        (isAnswerCorrect ? (
           <p className="text-green-500 text-lg font-semibold">
             You are correct!
           </p>
         ) : (
           <div className="mt-4 p-4 bg-gray-100 rounded">
             <h5 className="text-red-500">Incorrect!</h5>
-            {type === "math" ? (
-              <div className="math-explanation">
-                <Latex>{explanation || ""}</Latex>
-              </div>
-            ) : (
-              explanation
-            )}
+            {type === "math" ? parseContent(explanation || ""): explanation}
             {type === "math" &&
               imageUrls &&
               imageUrls.map((url, idx) => (
@@ -57,8 +44,7 @@ const Result: React.FC<ResultProps> = ({
                 />
               ))}
           </div>
-        )
-      )}
+        ))}
     </div>
   );
 };
