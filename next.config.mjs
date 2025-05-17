a15f503c1f44ec39625ed36c8496185cf54b77f9
Hemit Patel <hemitvpatel@gmail.com>
2025-05-17 09:33:51 -0400

    update next to remove experminental features + verisons

--- next.config.mjs
+++ next.config.mjs
@@ -3,10 +3,7 @@ const nextConfig = {
   reactStrictMode: false,
   images: {
     domains: ["cdn.kastatic.org", "yt3.ggpht.com", "images.unsplash.com", "lh3.googleusercontent.com", "plus.unsplash.com", "img.freepik.com"], // Add the hostnames here
-  },
-  experimental: {
-    nodeMiddleware: true,
-  },
+  }
 };
 
 export default nextConfig;
