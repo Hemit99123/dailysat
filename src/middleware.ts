ce3a30fe0e5890cdd81c4bd54e200844183e4e51
Hemit Patel <hemitvpatel@gmail.com>
2025-05-17 10:00:14 -0400

    updated logic (login protection is opposite of reg protection so in need of its own function) + moved the redirect outside of commons folder in libs/mw

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
-import handleProtectedRoutes from './lib/middleware/auth/protectedRoute';
+import {handleProtectedRoutes, handleSignInRoutes} from './lib/middleware/auth/protectedRoute';
 import protectedLoginRoutes from './data/protected-routes/protectedLoginRoutes';
 import protectedUserRoutes from './data/protected-routes/protectedUserRoutes';
 
@@ -9,7 +9,7 @@ export const middleware = async (request: NextRequest) => {
   if (userResponse) return userResponse;
   
 
-  const signinResponse = await handleProtectedRoutes(request, protectedLoginRoutes);
+  const signinResponse = await handleSignInRoutes(request, protectedLoginRoutes);
   if (signinResponse) return signinResponse;
   
 
