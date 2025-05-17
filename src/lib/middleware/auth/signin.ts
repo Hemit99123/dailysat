184b6b8f0f3c7068fe3b64ae414c17ea2f21f19b
Hemit Patel <hemitvpatel@gmail.com>
2025-05-16 23:12:03 -0400

    migrated to better auth here (was causing crypto error)

--- src/lib/middleware/auth/signin.ts
+++ src/lib/middleware/auth/signin.ts
@@ -1,10 +1,10 @@
-import { auth } from "@/lib/auth";
 import protectedRoutes from "@/data/protected-routes/protectedLoginRoutes";
 import type { NextRequest } from "next/server";
 import redirectTo from "../common/redirect";
+import { handleGetSession } from "@/lib/auth/authActions";
 
 const handleSignInRoutes = async (request: NextRequest) => {
-    const session = await auth();
+    const session = await handleGetSession();
   
     if (session && protectedRoutes.includes(request.nextUrl.pathname)) {
       return redirectTo(request, '/')
