e2246131c80c979daae8a945bb25dedf408a6322
Hemit Patel <hemitvpatel@gmail.com>
2025-05-17 09:47:14 -0400

    updated name where the SOLE function for protecting routes resides + removed all other files no longer needed

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
-import handleProtectedRoutes from './lib/middleware/auth/signin';
+import handleProtectedRoutes from './lib/middleware/auth/protectedRoute';
 import protectedLoginRoutes from './data/protected-routes/protectedLoginRoutes';
 import protectedUserRoutes from './data/protected-routes/protectedUserRoutes';
 
