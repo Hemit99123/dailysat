29bd2026cc6da576d0894fd3da632f4b7672246b
Hemit Patel <hemitvpatel@gmail.com>
2025-06-02 21:42:52 -0400

    changed to better auth implementation

--- src/app/tutor/page.tsx
+++ src/app/tutor/page.tsx
@@ -2,7 +2,7 @@
 
 import { useState } from 'react';
 import { ChevronDown, X } from 'lucide-react';
-import { handleSignIn } from "@/lib/auth/authAction";
+import { signIn } from "@/lib/auth/authClient";
 
 const Tutor = () => {
   const [activeFaq, setActiveFaq] = useState<number | null>(null);
@@ -112,7 +112,7 @@ const Tutor = () => {
                 <p className="text-gray-600 mb-6">
                   Join our community of expert tutors and start helping students excel on their SAT. Set your own hours and rates.
                 </p>
-                <button onClick={() => handleSignIn()} className="w-full bg-white border border-gray-300 rounded-md shadow-sm py-2 px-4 flex items-center justify-center text-sm font-medium text-gray-700 hover:bg-gray-50 mb-4">
+                <button onClick={signIn} className="w-full bg-white border border-gray-300 rounded-md shadow-sm py-2 px-4 flex items-center justify-center text-sm font-medium text-gray-700 hover:bg-gray-50 mb-4">
                   <svg className="h-5 w-5 mr-2" viewBox="0 0 24 24" width="24" height="24" xmlns="http://www.w3.org/2000/svg">
                     <g transform="matrix(1, 0, 0, 1, 27.009001, -39.238998)">
                       <path fill="#4285F4" d="M -3.264 51.509 C -3.264 50.719 -3.334 49.969 -3.454 49.239 L -14.754 49.239 L -14.754 53.749 L -8.284 53.749 C -8.574 55.229 -9.424 56.479 -10.684 57.329 L -10.684 60.329 L -6.824 60.329 C -4.564 58.239 -3.264 55.159 -3.264 51.509 Z"/>
