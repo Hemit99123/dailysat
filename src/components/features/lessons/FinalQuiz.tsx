f0f4db528104e2b5110a330bdc748d1b3f21b586
Achyuth Suresh Nair <achyuthsureshnair81@gmail.com>
2025-07-18 18:02:10 -0700

    fix: syntax error

--- src/components/features/lessons/FinalQuiz.tsx
+++ src/components/features/lessons/FinalQuiz.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React from "react";
-import { mathQuiz, readingWritingQuiz } from "@/data/FinalQuiz";
+import { mathQuiz, readingWritingQuiz } from "@/data/finalquiz";
 
 interface FinalQuizProps {
   quizType: "math" | "reading";
