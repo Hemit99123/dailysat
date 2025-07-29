cada236078c0e037a4519a3ae3f466eb9afb6725
Hemit Patel <hemitvpatel@gmail.com>
2025-07-28 21:02:41 -0400

    remove the redirection logic so that page actually serves content on /practice

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,7 +3,6 @@ import type { NextRequest } from 'next/server';
 import {handleProtectedRoutes, handleSignInRoutes} from '@/lib/middleware/auth/protectedRoute';
 import protectedLoginRoutes from './data/protected-routes/protectedLoginRoutes';
 import protectedUserRoutes from './data/protected-routes/protectedUserRoutes';
-import redirectTo from "@/lib/middleware/redirect";
  
 
 export const middleware = async (request: NextRequest) => {
@@ -14,7 +13,5 @@ export const middleware = async (request: NextRequest) => {
   const signinResponse = await handleSignInRoutes(request, protectedLoginRoutes);
   if (signinResponse) return signinResponse;
   
-  if (request.nextUrl.pathname === "/practice") return redirectTo(request, "/practice/math")
-
   return NextResponse.next();
 };
