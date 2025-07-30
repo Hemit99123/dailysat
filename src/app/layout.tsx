1535d755f97d6b37ff8a10b0d36dc7521e80d373
Hemit Patel <hemitvpatel@gmail.com>
2025-07-30 09:33:13 -0400

    hydration errors so moved everything to other component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import { GoogleOAuthProvider } from "@react-oauth/google";
-import { Analytics } from "@vercel/analytics/next";
-import { ToastContainer } from "react-toastify";
 import 'katex/dist/katex.min.css';
-import NavBar from "@/components/common/NavBar";
+import Providers from "@/components/common/Providers";
 
 export const metadata: Metadata = {
   title: "DailySAT",
@@ -16,14 +13,9 @@ export default function RootLayout({ children }: React.PropsWithChildren) {
   return (
     <html lang="en">
       <body>
-        <GoogleOAuthProvider
-          clientId={process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID as string}
-        >
-          <NavBar />
-          {children}
-        </GoogleOAuthProvider>
-        <Analytics />
-        <ToastContainer></ToastContainer>
+        <Providers
+          children={children}
+        />
       </body>
     </html>
   );
