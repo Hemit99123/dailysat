07d34a50336f9c10074ff1e5c84023f60f98c9bf
Hemit Patel <hemitvpatel@gmail.com>
2025-07-29 21:03:19 -0400

    added basic cta file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
 "use client";
 
+import React from 'react'
 import Footer from '@/components/common/Footer'
 import FAQ from '@/components/features/Landing-Page/FAQ';
 import Features from '@/components/features/Landing-Page/Features'
+import FinalCTA from '@/components/features/Landing-Page/FinalCTA';
 import Hero from '@/components/features/Landing-Page/Hero'
 import Testimonials from '@/components/features/Landing-Page/Testimonials'
 import Workshop from '@/components/features/Landing-Page/Workshop';
-import React from 'react'
 
 const LandingPage = () => {
   return (
@@ -17,6 +18,7 @@ const LandingPage = () => {
             <Testimonials />
             <Workshop />
             <FAQ />
+            <FinalCTA />
         </div>
         <Footer />
     </div>
