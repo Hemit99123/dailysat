11f69c75f9534272dae35d98983ed786fcdaefa6
Hemit Patel <hemitvpatel@gmail.com>
2025-07-30 21:15:04 -0400

    added investor logic (if user has investors then that bonus will be added)

--- src/lib/server-actions/submitQuestionAction.ts
+++ src/lib/server-actions/submitQuestionAction.ts
@@ -3,6 +3,7 @@
 import { QUESTION_IS_CORRECT_POINTS } from '@/data/constant';
 import { client } from '@/lib/mongo';
 import { handleGetSession } from '@/lib/auth/authActions';
+import { InvestorItem } from '@/types/server-actions/investor-item';
 
 export const handleSubmitQuestion = async (isCorrect: boolean) => {
   try {
@@ -17,12 +18,26 @@ export const handleSubmitQuestion = async (isCorrect: boolean) => {
 
     const db = client.db('DailySAT');
     const usersColl = db.collection('users');
+    let investorRewardBonus = 0;
 
+    const investorItem = await usersColl.findOne<InvestorItem>(
+      { email },
+      { projection: { investors: 1 } } 
+    );
+
+    // Calculate earnings for each investor
+    investorItem?.investors.map((investor) => {
+        const amnt = investor.amnt ?? 1;
+        const reward = investor.reward ?? 0;
+        const quantity = amnt * reward;
+        investorRewardBonus += quantity;
+    });
+    
     await usersColl.updateOne(
       { email },
       {
         $inc: {
-          currency: isCorrect ? QUESTION_IS_CORRECT_POINTS : 0,
+          currency: isCorrect ? QUESTION_IS_CORRECT_POINTS + investorRewardBonus : 0,
           correctAnswered: isCorrect ? 1 : 0,
           wrongAnswered: !isCorrect ? 1 : 0,
         },
