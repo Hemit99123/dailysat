f4889c818269314efbb71c39802a375eb5698b70
Hemit Patel <hemitvpatel@gmail.com>
2025-08-04 17:06:11 -0400

    change page contents a bit

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,9 +1,9 @@
 const NotFound = () => {
   return (
     <div className="flex flex-col justify-center items-center h-[calc(100vh-5rem)] px-4 text-center">
-      <p className="text-sm font-semibold text-blue-600">404 error</p>
+      <p className="text-sm font-semibold text-blue-600">404!</p>
       <h1 className="mt-2 text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
-        Page not found 😕
+        Page not found 
       </h1>
       <p className="mt-4 text-base text-gray-600">
         Sorry, we couldn&apos;t find the page you&apos;re looking for.
