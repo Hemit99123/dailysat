4e1c12d8a978aa5ec5859a36b68b4ecd49fb6c7a
Hemit Patel <hemitvpatel@gmail.com>
2025-08-03 22:08:47 -0400

    add other types

--- src/types/practice/questions.ts
+++ src/types/practice/questions.ts
@@ -20,3 +20,35 @@ export type QuestionHistory = {
   isMarkedForLater: boolean;
   isAnswered: boolean;
 };
+
+export interface QuestionData {
+  questionMeta: {
+    _id: string;
+    key: string;
+    id: string;
+    visuals: {
+      type: string;
+      svg_content: string;
+    };
+    question: {
+      choices: Record<string, string>;
+      question: string;
+      paragraph?: string;
+      explanation: string;
+      correct_answer: string;
+    };
+    difficulty: string;
+    subject: string;
+  };
+}
+
+export interface RawStemQuestion {
+  choices: Record<string, string>;
+  question: string;
+  paragraph?: string;
+  explanation: string;
+  correct_answer: string;
+  stemHtml: string;
+  stem: string;
+  prompt: string;
+}
