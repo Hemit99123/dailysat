3d2afa15dccf484aedc3dae71bbbe36ab8ef408f
Hemit Patel <hemitvpatel@gmail.com>
2025-08-03 22:03:21 -0400

    add proper types

--- src/types/practice/questions.ts
+++ src/types/practice/questions.ts
@@ -1,31 +1,22 @@
-export interface QuestionData {
-  questionMeta: {
-    _id: string;
-    key: string;
-    id: string;
-    visuals: {
-      type: string;
-      svg_content: string;
-    };
-    question: {
-      choices: Record<string, string>;
-      question: string;
-      paragraph?: string;
-      explanation: string;
-      correct_answer: string;
-    };
-    difficulty: string;
-    subject: string;
+export type Question = {
+  id: string;
+  domain: string;
+  visuals: { type: string; svg_content: string };
+  question: {
+    choices: Record<string, string>;
+    question: string;
+    paragraph: string | null;
+    explanation: string;
+    correct_answer: string;
   };
-}
+  difficulty: "Easy" | "Medium" | "Hard" | string;
+};
 
-export interface RawStemQuestion {
-  choices: Record<string, string>;
-  question: string;
-  paragraph?: string;
-  explanation: string;
-  correct_answer: string;
-  stemHtml: string;
-  stem: string;
-  prompt: string;
-}
+export type QuestionHistory = {
+  id: number;
+  question: Question;
+  userAnswer: string | null;
+  isCorrect: boolean | null;
+  isMarkedForLater: boolean;
+  isAnswered: boolean;
+};
