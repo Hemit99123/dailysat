62479ad53badc47e77b96d8463951dc6ca120a71
Hemit Patel <hemitvpatel@gmail.com>
2025-08-16 12:51:15 -0400

    complete moving the studydata type

--- src/app/ai/page.tsx
+++ src/app/ai/page.tsx
@@ -5,10 +5,8 @@ import type React from "react"
 import { useState } from "react"
 import { generateStudyPlan } from "@/lib/ai/generateStudyPlan"
 import { StudyPlan } from "@/components/features/AI-Planner/StudyPlan"
-import { ValidPlan, DebugPlan, ErrorPlan, StudyDay } from "@/types/ai-planner/ai"
+import { StudyPlanData, StudyDay } from "@/types/ai-planner/ai"
 import {toast} from "react-toastify"
-
-type StudyPlanData = ValidPlan | DebugPlan | ErrorPlan
 
 const AI = () => {
   const [currentScore, setCurrentScore] = useState("");
